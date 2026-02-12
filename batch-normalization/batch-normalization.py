c1b7e1e0c69902d384046d80ff7ff542feabf345
谢乃闻 <sienaiwun@users.noreply.github.com>
2026-02-12 19:52:53 +0000

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -9,7 +9,7 @@ def batch_norm_forward(x, gamma, beta, eps=1e-5):
     beta = np.array(beta)
     if npx.ndim == 2:
         pu = np.mean(x, axis=0) #2D features
-        delta_squear = np.mean((x -pu)**2, axis = 0)
+        delta_squear = np.mean((x -pu)**2, axis = 0, keepdims=True)
         x_hat = (x - pu)/np.sqrt(delta_squear + eps)
         return gamma* x_hat + beta
     else:
