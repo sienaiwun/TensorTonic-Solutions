c02e07c8000f46b70a7658d093737da9f15e0c92
谢乃闻 <sienaiwun@users.noreply.github.com>
2026-02-12 21:46:35 +0000

    Update solution for maxpool-forward

--- maxpool-forward/maxpool-forward.py
+++ maxpool-forward/maxpool-forward.py
@@ -1,24 +1,14 @@
 import numpy as np
 def maxpool_forward(X, pool_size, stride):
-   
-
     X = np.array(X)
-    H, W = X.shape  # height, width
+    H,W  = X.shape
     
-    Hout = (H - pool_size) // stride + 1
-    Wout = (W - pool_size) // stride + 1
-    
-    out = np.empty((Hout, Wout), dtype=X.dtype)
-    
-    for h in range(Hout):
-        for w in range(Wout):
+    Wout = int(np.floor((W - pool_size) / stride)) + 1
+    Hout = int(np.floor((H - pool_size) / stride)) + 1
+    out = np.empty((Hout,Wout),dtype = X.dtype)
+    for w in range(Wout):
+        for h in range(Hout):
             h_start = h * stride
             w_start = w * stride
-            window = X[h_start:h_start+pool_size, w_start:w_start+pool_size]
-            out[h, w] = np.max(window)
-    
+            out[h,w] = np.max(X[h_start:h_start+pool_size,w_start:w_start+pool_size])
     return out.tolist()
-    """
-    Compute the forward pass of 2D max pooling.
-    """
-    # Write code here
